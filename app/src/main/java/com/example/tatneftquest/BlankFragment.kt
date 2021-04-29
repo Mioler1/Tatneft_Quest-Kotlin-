31abf0983bdb7279c644a06bacfa24f9db4dece3
Galdin Sergey <veretennik-v@mail.ru>
2021-04-29 12:32:37 +0300

    new branch

---
+++ app/src/main/java/com/example/tatneftquest/BlankFragment.kt
@@ -0,0 +1,28 @@
+package com.example.tatneftquest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.tatneftquest.databinding.FragmentBlankBinding
+
+class BlankFragment : Fragment() {
+    private lateinit var binding: FragmentBlankBinding
+    private lateinit var textView: TextView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBlankBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView = binding.myText
+
+    }
+}

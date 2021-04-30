d88675bd288f6aaa3779572d1a123be68250a932
Galdin Sergey <veretennik-v@mail.ru>
2021-05-01 00:14:58 +0300

    added interface switching fragment

--- app/src/main/java/com/example/tatneftquest/MainActivity.kt
+++ app/src/main/java/com/example/tatneftquest/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.tatneftquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.tatneftquest.Menu.AppDrawer
 import com.example.tatneftquest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ReplaceFragmentHandler {
     lateinit var mBinding: ActivityMainBinding
     lateinit var mToolbar: Toolbar
     private lateinit var appDrawer: AppDrawer
@@ -23,4 +27,9 @@ class MainActivity : AppCompatActivity() {
         mToolbar = mBinding.toolbar
         appDrawer = AppDrawer(this, mToolbar)
     }
+
+    override fun replace(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+    }
 }
+

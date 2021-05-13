072d712546c3ca9ae354866d2a9987ea4c4e3248
Mioler1 <graveer3759@mail.ru>
2021-05-13 19:22:21 +0300

    added check mistakes authorization and registration

--- app/src/main/java/com/example/tatneftquest/fragments/AuthorizationFragment.kt
+++ app/src/main/java/com/example/tatneftquest/fragments/AuthorizationFragment.kt
@@ -2,21 +2,27 @@ package com.example.tatneftquest.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 
 import com.example.tatneftquest.MainActivity
 import com.example.tatneftquest.Slider.SliderActivity
 import com.example.tatneftquest.databinding.FragmentAuthorizationBinding
+import java.util.concurrent.Executors
 
 
 class AuthorizationFragment : Fragment() {
     private lateinit var binding: FragmentAuthorizationBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View {
         binding = FragmentAuthorizationBinding.inflate(inflater)
         return binding.root
@@ -24,9 +30,26 @@ class AuthorizationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val intent = Intent(context, SliderActivity::class.java)
+
         binding.comeButton.setOnClickListener {
-            val intent = Intent(context, SliderActivity::class.java)
+            val numberText = binding.numberAuthorization.text.toString()
+            val passwordText = binding.passwordAuthorization.text.toString()
+            if (numberText.isEmpty()) {
+                binding.textInputNumber.error = "Пустое поле"
+                return@setOnClickListener
+            }
+            if (numberText.length < 10) {
+                binding.textInputNumber.error = "Номер телефона введен неверно"
+                return@setOnClickListener
+            }
+            if (passwordText.isEmpty()) {
+                binding.textInputPassword.error = "Пустое поле"
+                binding.textInputNumber.error = null
+                binding.textInputPassword.errorIconDrawable = null
+                return@setOnClickListener
+            }
             startActivity(intent)
         }
-    }
+}
 }
--- app/src/main/java/com/example/tatneftquest/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/example/tatneftquest/fragments/RegistrationFragment.kt
@@ -1,24 +1,78 @@
 package com.example.tatneftquest.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tatneftquest.FirstScreenActivity
 import com.example.tatneftquest.R
+import com.example.tatneftquest.Slider.SliderActivity
+import com.example.tatneftquest.databinding.FragmentAuthorizationBinding
+import com.example.tatneftquest.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentRegistrationBinding
+    var check = false
 
-    }
+    val passwordCheck =
+        """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#${'$'}%!\-_?&])(?=\S+${'$'}).{8,}""".toRegex()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentRegistrationBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val intent = Intent(context, SliderActivity::class.java)
+
+        binding.regButton.setOnClickListener {
+            val numberText = binding.numberRegistration.text.toString()
+            val passwordText = binding.passwordRegistration.text.toString()
+            val repeatPassword = binding.repeatPasswordRegistration.text.toString()
+            if (numberText.isEmpty()) {
+                binding.textInputNumber.error = "Пустое поле"
+                return@setOnClickListener
+            }
+            if (passwordText.isEmpty()) {
+                binding.textInputPassword.error = "Пустое поле"
+                binding.textInputNumber.error = null
+                return@setOnClickListener
+            }
+            if (passwordText.matches(passwordCheck)) {
+                check = true
+            }
+            if (!check) {
+                binding.textInputPassword.error = "Заглавная буква, цифра, спец. символ"
+                return@setOnClickListener
+            }
+            if (repeatPassword.isEmpty()) {
+                binding.textInputRepeatPassword.error = "Пустое поле"
+                binding.textInputPassword.error = null
+                return@setOnClickListener
+            }
+            if (numberText.length < 10) {
+                binding.textInputNumber.error = "Номер телефона введен неверно"
+                binding.textInputPassword.error = null
+                return@setOnClickListener
+            }
+            if (passwordText.length < 6) {
+                binding.textInputPassword.error = "Минимум 6 символов"
+                return@setOnClickListener
+            }
+            if (passwordText != repeatPassword) {
+                binding.textInputRepeatPassword.error = "Пароль не совпадают"
+                return@setOnClickListener
+            }
+            startActivity(intent)
+        }
+
     }
 
 }

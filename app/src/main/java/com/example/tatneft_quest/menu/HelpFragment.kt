8063bd064321adf63c20dc4b9aa7821fe654d123
Mioler1 <graveer3759@mail.ru>
2021-06-03 14:03:47 +0300

    version 1.0.0.1

---
+++ app/src/main/java/com/example/tatneft_quest/menu/HelpFragment.kt
@@ -0,0 +1,29 @@
+package com.example.tatneft_quest.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tatneft_quest.R
+import com.example.tatneft_quest.databinding.FragmentHelpBinding
+
+class HelpFragment : Fragment() {
+
+    private lateinit var binding: FragmentHelpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentHelpBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Помощь"
+    }
+
+}
